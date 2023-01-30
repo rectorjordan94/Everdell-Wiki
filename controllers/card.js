6f8de67f6c2a388ae6efb625f0d4e52a4f01eee2
rectorjordan94 <rectorjordan94@gmail.com>
2023-01-29 19:52:06 -0600

    updates styling for navbar and allows nonloggedin users to view all pages except for new and edit

--- controllers/card.js
+++ controllers/card.js
@@ -8,16 +8,16 @@ const router = express.Router()
 // Router Middleware
 // Authorization middleware
 // If you have some resources that should be accessible to everyone regardless of loggedIn status, this middleware can be moved, commented out, or deleted. 
-router.use((req, res, next) => {
-	// checking the loggedIn boolean of our session
-	if (req.session.loggedIn) {
-		// if they're logged in, go to the next thing(thats the controller)
-		next()
-	} else {
-		// if they're not logged in, send them to the login page
-		res.redirect('/auth/login')
-	}
-})
+// router.use((req, res, next) => {
+// 	// checking the loggedIn boolean of our session
+// 	if (req.session.loggedIn) {
+// 		// if they're logged in, go to the next thing(thats the controller)
+// 		next()
+// 	} else {
+// 		// if they're not logged in, send them to the login page
+// 		res.redirect('/auth/login')
+// 	}
+// })
 
 // Routes
 
--- controllers/search.js
+++ controllers/search.js
@@ -5,13 +5,27 @@ const Card = require('../models/card')
 // Create router
 const router = express.Router()
 
+const normalizeText = (text) => {
+    const words = text.split(' ')
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i][0].toUpperCase() + words[i].substr(1)
+    }
+    const name = words.join(' ')
+    return name
+}
+
 router.get('/', function(req, res) {
     const name = req.query.name
-    Card.findOne().where({ name: `${name}` })
+    Card.findOne().where({ name: `${normalizeText(name)}` })
         .then(card => {
             res.redirect(`/cards/${card.id}`)
         })
-        .catch(err => console.log(err))
+        .catch((error) => {
+            console.log(error)
+			res.status(400)
+		})
 })
+
+normalizeText('chip sweep')
 
 module.exports = router
